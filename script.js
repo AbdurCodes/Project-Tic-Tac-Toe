8e6a704e955ed7bb1f1a13f8aec22f5b6de701c5
abdurcodes <aravman@gmail.com>
2024-10-01 11:51:56 +0500

    setup the gameboard in the page center

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('Hi');
+const cells = document.querySelectorAll('.cell');
+
+const cellsArray = Array.from(cells);
+cellsArray.forEach((box, index)=>{
+    box.addEventListener('click', ()=> {
+        alert(`Box ${index+1} clicked!`);
+    })
+})
